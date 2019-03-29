9ccb85c9b7a9e7ed197f178180ed9410a6c85ccf
Ulrik Landberg Stephansen <ulrikls@gmail.com>
2019-03-29 23:21:32 +0100

    Tutorial app

---
+++ mobileclient/app/src/main/java/dk/moveyourfeet/mobileclient/DisplayMessageActivity.kt
@@ -0,0 +1,22 @@
+package dk.moveyourfeet.mobileclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_display_message)
+
+    // Get the Intent that started this activity and extract the string
+    val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+    // Capture the layout's TextView and set the string as its text
+    findViewById<TextView>(R.id.textView).apply {
+      text = message
+    }
+
+  }
+}
--- mobileclient/app/src/main/java/dk/moveyourfeet/mobileclient/MainActivity.kt
+++ mobileclient/app/src/main/java/dk/moveyourfeet/mobileclient/MainActivity.kt
@@ -1,7 +1,12 @@
 package dk.moveyourfeet.mobileclient
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "dk.moveyourfeet.mobileclient.MESSAGE"
 
 class MainActivity : AppCompatActivity()
 {
@@ -11,4 +16,16 @@ class MainActivity : AppCompatActivity()
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
+
+  /** Called when the user taps the Send button */
+  fun sendMessage(view: View) {
+    val editText = findViewById<EditText>(R.id.editText)
+    val message = editText.text.toString()
+    val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+      putExtra(EXTRA_MESSAGE, message)
+    }
+    startActivity(intent)
+
+  }
+
 }
