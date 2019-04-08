72070c3bc911c1d6f81a6baba5f7437570a661de
Ulrik Landberg Stephansen <ulrikls@gmail.com>
2019-04-08 21:01:31 +0200

    iOS Xcode project

---
+++ mobileclient/ios/MoveYourFeet/MoveYourFeet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoveYourFeet
+//
+//  Created by Ulrik Landberg Stephansen on 08/04/2019.
+//  Copyright © 2019 Move Your Feet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ mobileclient/ios/MoveYourFeet/MoveYourFeetTests/MoveYourFeetTests.swift
@@ -0,0 +1,34 @@
+//
+//  MoveYourFeetTests.swift
+//  MoveYourFeetTests
+//
+//  Created by Ulrik Landberg Stephansen on 08/04/2019.
+//  Copyright © 2019 Move Your Feet. All rights reserved.
+//
+
+import XCTest
+@testable import MoveYourFeet
+
+class MoveYourFeetTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
